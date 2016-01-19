f076b5cd564cfae6dbd6b7c89236e665dab772bf
viswateja nelakuditi <nvteja@viswatejas-MacBook-Pro.local>
2016-01-19 16:32:18 -0600

    first version

--- GuessDaWord/ViewController.swift
+++ GuessDaWord/ViewController.swift
@@ -10,6 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var GuessText: UITextField!
+    
+    @IBOutlet weak var GuessDisplay: UILabel!
+    
+    @IBAction func CheckGuess(sender: AnyObject){
+        
+        
+        let randomWord:String = "BEARS"
+        let gText = GuessText.text!.uppercaseString
+        var guess = Array(gText.characters)
+        var displayText = GuessDisplay.text!
+        var randomWordArray = Array(randomWord.characters)
+        
+        if(guess.count != 5){
+            
+            displayText = displayText + "\n" + gText + " = Please enter only 5 letter words!"
+            GuessDisplay.text = displayText
+            return
+        }
+        
+        var alpha: Int = 0 , beta:Int = 0
+        
+        var i = 0;
+        for(i = 0; i < 5; i++){
+        
+            let guessIndex=randomWordArray.indexOf(guess[i])
+            if(guessIndex >= 0){
+            
+                if(String(guess[i]) == String(randomWordArray[i])){ // alpha
+                    
+                  alpha = alpha + 1
+           
+                } else{ // beta
+                  
+                    beta = beta + 1
+                    
+                }
+
+            }
+            
+        }
+        if(alpha == 5){
+        
+            displayText = displayText + "\n" + gText + " You guessed the word!"
+            
+        } else{
+            
+            displayText = displayText + "\n" + gText + " = \(alpha) Alpha, \(beta) Beta"
+        }
+        GuessDisplay.text = displayText
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
